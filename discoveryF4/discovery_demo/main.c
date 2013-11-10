1313b0a8ba52a40e939d593c1d52165ce312c71a
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-11-10 20:53:37 +0800

    extend LED display to 12 LEDs for 12 bits
    LEDs show a bit map of sampled data.
    
    Issue: Macro makes it hard to code this in loop.

--- discoveryF4/discovery_demo/main.c
+++ discoveryF4/discovery_demo/main.c
@@ -93,17 +93,33 @@ int main(void)
 
     while(1){//loop while the board is working
       ConvertedValue = adc_convert();//Read the ADC converted value
-      if ((ConvertedValue > 2000) && (ConvertedValue < 2500))
-      {
-        GPIO_SetBits(GPIOE , GPIO_Pin_11);
-        STM_EVAL_LEDOn(LED4);
-        Delay(100);
-      }
-      else{
-        GPIO_ResetBits(GPIOE , GPIO_Pin_11);
-        STM_EVAL_LEDOff(LED4);
-        Delay(100);
-      }
+      i = 0;
+      if (ConvertedValue & (1 << 0))
+        i |= GPIO_Pin_3;
+      if (ConvertedValue & (1 << 1))
+        i |= GPIO_Pin_4;
+      if (ConvertedValue & (1 << 2))
+        i |= GPIO_Pin_5;
+      if (ConvertedValue & (1 << 3))
+        i |= GPIO_Pin_6;
+      if (ConvertedValue & (1 << 4))
+        i |= GPIO_Pin_7;
+      if (ConvertedValue & (1 << 5))
+        i |= GPIO_Pin_8;
+      if (ConvertedValue & (1 << 6))
+        i |= GPIO_Pin_9;
+      if (ConvertedValue & (1 << 7))
+        i |= GPIO_Pin_10;
+      if (ConvertedValue & (1 << 8))
+        i |= GPIO_Pin_11;
+      if (ConvertedValue & (1 << 9))
+        i |= GPIO_Pin_12;
+      if (ConvertedValue & (1 << 10))
+        i |= GPIO_Pin_13;
+      if (ConvertedValue & (1 << 11))
+        i |= GPIO_Pin_14;
+      GPIO_ResetBits(GPIOE, i);
+      Delay(100);
     }
 
 /* Try to test ADC.*/
@@ -147,9 +163,9 @@ int adc_convert(){
 void GPIO_PIN_INIT(void){
     GPIO_InitTypeDef GPIO_InitStructure;
     
-    GPIO_PinAFConfig(GPIOE, GPIO_PinSource11, GPIO_AF_TIM3);
+    GPIO_PinAFConfig(GPIOE, GPIO_PinSource3|GPIO_PinSource4|GPIO_PinSource5|GPIO_PinSource6|GPIO_PinSource7|GPIO_PinSource8|GPIO_PinSource9|GPIO_PinSource10|GPIO_PinSource11|GPIO_PinSource12|GPIO_PinSource13|GPIO_PinSource14, GPIO_AF_TIM3);
     
-    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;            // Alt Function - Push Pull
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100
